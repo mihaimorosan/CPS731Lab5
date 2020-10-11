9b17a64cbb101acd600c39d103a779be77732dfa
mihaimorosan <morosanmihai@live.com>
2020-10-10 23:55:41 -0400

    Added Splash Screen, splash screen audio and ability to stop audio after 5 seconds

---
+++ app/src/main/java/com/example/lab5/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+    MediaPlayer song;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        song = new MediaPlayer();
+        song = MediaPlayer.create(this,R.raw.song);
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                song.stop();
+                startActivity(new Intent(SplashScreen.this, CarListActivity.class));
+            }
+        };
+        song.start();
+        Timer opening = new Timer();
+        opening.schedule(task, 5000);
+    }
+}
